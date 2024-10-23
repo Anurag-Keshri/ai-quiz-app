fd672a3999d30dffc847289061d222f645f46c36
Anurag Keshri <anuragkeshri47@gmail.com>
2024-10-24 03:56:24 +0530

    fix: redirect correctly to landing page

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
 	return view('landing');
 })->name('landing');
 
-Route::get('/dashboard', function () {
-	return view('dashboard');
-})->name('dashboard');
-
 require __DIR__.'/auth.php';
 require __DIR__.'/profile.php';
 require __DIR__.'/quiz.php';
