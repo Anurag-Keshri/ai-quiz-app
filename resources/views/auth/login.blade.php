d9eea26fbf7ee9b8d598086ad2dd2ff928c0d52a
Anurag Keshri <anuragkeshri47@gmail.com>
2024-10-24 03:45:32 +0530

    style: add register/signup route to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,20 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="grid grid-cols-2 items-center  mt-4">
+			<div class="items-center text-center">
+				<p class="text-sm text-gray-600 dark:text-gray-400">
+					{{ __("Don't have an account?") }}
+					<a href="{{ route('register') }}" class="underline text-sm text-indigo-600 dark:text-indigo-400 hover:text-indigo-900 dark:hover:text-indigo-300">
+						{{ __('Sign up') }}
+					</a>
+				</p>
+			</div>
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 justify-self-end">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
     </form>
+
 </x-guest-layout>
