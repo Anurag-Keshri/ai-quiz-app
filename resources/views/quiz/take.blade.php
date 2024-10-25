18dd8fab69f7d6c2fc63706d63c148ce9f5f59cc
Anurag Keshri <anuragkeshri47@gmail.com>
2024-10-25 06:20:31 +0530

    feat: implement shuffle questions and shuffle options

--- resources/views/quiz/take.blade.php
+++ resources/views/quiz/take.blade.php
@@ -6,12 +6,25 @@
         <h1 class="self-center w-full text-2xl font-bold text-center text-gray-900 dark:text-gray-100 p-4 bg-gray-50 dark:bg-gray-800 rounded-lg  border-gray-300 dark:border-gray-600">
             Taking Quiz: {{ $quiz->title }}
         </h1>
-
-        @foreach ($quiz->questions as $question)
+		@php
+			if ($quiz->shuffle_questions) {
+				$questions = $quiz->questions->shuffle();
+			} else {
+				$questions = $quiz->questions;
+			}
+		@endphp
+        @foreach ($questions as $question)
             <div class="bg-white dark:bg-gray-800 rounded-lg shadow-md p-4 ">
                 <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">{{ $loop->iteration }}. {{ $question->question_text }}</h3>
                 <ul class="mt-2 space-y-2">
-                    @foreach (json_decode($question->options) as $option) <!-- Decode JSON options -->
+                    @php
+                        if ($quiz->shuffle_options) {	
+                            $options = collect(json_decode($question->options))->shuffle();
+                        } else {
+                            $options = json_decode($question->options);
+                        }
+                    @endphp
+                    @foreach ($options as $option) <!-- Decode JSON options -->
                         <li>
                             <label class="flex items-center p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700 transition duration-150">
                                 <input type="radio" name="question_{{ $question->id }}" value="{{ $option }}" class="mr-2" required>
